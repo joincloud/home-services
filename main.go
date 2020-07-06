3b34ce29fda0ee0ba32b632a1b2891886fd9bdfb
shu xian <i@shux.me>
2020-07-06 21:59:19 +0800

    get addr

--- main.go
+++ main.go
@@ -2,18 +2,25 @@ package main
 
 import (
 	"context"
+
 	"github.com/joincloud/home-platform/home-services/conf"
 	"github.com/joincloud/home-platform/home-services/node"
 	"github.com/joincloud/home-platform/home-services/registry"
+	log "github.com/sirupsen/logrus"
 )
 
 func main() {
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
-	conf.Init("E:\\GOPATH\\src\\github.com\\printfcoder\\home-services\\application.yml")
+	conf.Init("/home/sx/Project/home/home-services/application.yml")
 	node.Init(ctx)
 	registry.Register(ctx)
 
-	node.GetWriteTo(ctx, "tmp", "QmUaoioqU7bxezBQZkUcgcSyokatMY71sxsALxQmRRrHrj", "E:\\Projects\\home\\files\\save\\hello.txt")
+	addrs, err := node.GetNodeAddrs(ctx, "tmp")
+	if err != nil {
+		log.Error(err)
+	}
+
+	log.Info(addrs)
 }
--- node/node_api.go
+++ node/node_api.go
@@ -6,12 +6,28 @@ import (
 
 	files "github.com/ipfs/go-ipfs-files"
 	icorepath "github.com/ipfs/interface-go-ipfs-core/path"
+	ma "github.com/multiformats/go-multiaddr"
 	log "github.com/sirupsen/logrus"
 )
+
+func GetNodeAddrs(ctx context.Context, apiName string) (addrs []ma.Multiaddr, err error) {
+	if api, ok := node.APIs[apiName]; ok {
+		addrs, err := api.ipfs.Swarm().LocalAddrs(ctx)
+		if err != nil {
+			log.Error(err)
+			return nil, err
+		}
+		return addrs, nil
+	} else {
+		return nil, fmt.Errorf("api %s is nonexisted", apiName)
+	}
+}
 
 func GetWriteTo(ctx context.Context, apiName, fileID, outPath string) (err error) {
 	if api, ok := node.APIs[apiName]; ok {
 		cID := icorepath.New(fileID)
+		log.Infof("try to get file %s from %s", fileID, cID)
+
 		fileNode, err := api.ipfs.Unixfs().Get(ctx, cID)
 		if err != nil {
 			log.Error(err)
